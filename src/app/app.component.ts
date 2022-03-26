bfc8418a656ae90fe12093525c1732cf7fd0793e
Murat <mrdudov@gmail.com>
2022-03-26 16:50:19 +0300

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  title = 'Initial'
 
-  onInput(event: any) {
-    this.title = event.target.value
-  }
+  backgroundToggle = false
 
 }
