df99717a8b8a27e06d6a7b4d5f45e88c05fb1101
Murat <mrdudov@gmail.com>
2022-03-26 13:58:42 +0300

    Event Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  title = 'Dynamic title';
-  number = 42;
-  arr = [1,2,3];
-  obj = {
-    a: 1,
-    b: {
-      c:2
-    }
-  }
-  img = 'https://banner2.cleanpng.com/20180518/ptw/kisspng-react-logo-javascript-front-and-back-ends-user-int-5afef575942028.3034008315266584216067.jpg'
+  inputValue = '';
+  constructor() {}
 
-  constructor() {
-    setTimeout(() => {
-      this.img = 'https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/21_Angular-512.png'
-    }, 3000)
+  onInpunt(event: any) {
+    this.inputValue = (<HTMLInputElement>event.target).value;
   }
 
-  
+  onClick() {
+    console.log('clik me button');
+  }
+
+  onBlure(srt: string) {
+    this.inputValue = srt;
+  }
+
 }
