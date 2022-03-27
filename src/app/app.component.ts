90dff47e71a43bbf548fc0a713b2178bf8f42c16
Murat <mrdudov@gmail.com>
2022-03-27 11:48:13 +0300

    components ChangeDetectionStrategy.OnPush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export interface Post {
   title: string
@@ -11,11 +11,23 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   posts:(Post | undefined)[] = [
     {title: 'Title 1', text: 'text 1', id:1},
-    {title: 'Title 2', text: 'text 2', id:2}
+    // {title: 'Title 2', text: 'text 2', id:2}
   ]
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('Timeout')
+      this.posts[0]! = {
+        title: 'Changed!',
+        text: 'Changed text',
+        id: 4
+      }
+    }, 5000)
+  }
 
   updatePosts(post?: Post) {
     this.posts.unshift(post)
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -3,6 +3,7 @@ import {
   AfterContentInit,
   AfterViewChecked,
   AfterViewInit,
+  ChangeDetectionStrategy,
   Component, 
   ContentChild, 
   DoCheck, 
@@ -20,7 +21,8 @@ import { Post } from '../app.component';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostComponent implements 
   OnInit, 
