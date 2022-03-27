f169e22d9bb1870c60dc6e9054a0b6ae1dbaaa36
Murat <mrdudov@gmail.com>
2022-03-28 00:49:15 +0300

    services scope

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
