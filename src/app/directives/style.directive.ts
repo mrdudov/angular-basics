b2984c90522bfe0d6d8fd944e0eae43a8ebc1c30
Murat <mrdudov@gmail.com>
2022-03-27 15:27:17 +0300

    directives HostListener

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[appStyle]'
@@ -6,7 +6,18 @@ import { Directive, ElementRef, Renderer2 } from "@angular/core";
 export class StyleDirective {
 
     constructor(private el: ElementRef, private r: Renderer2) {
+        // this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+    }
+
+    @HostListener('click', ['$event.target']) onClick(event: Event) {
+        console.log(event)
+    }
+
+    @HostListener('mouseenter') onEnter() {
         this.r.setStyle(this.el.nativeElement, 'color', 'blue')
     }
 
+    @HostListener('mouseleave') onLeave() {
+        this.r.setStyle(this.el.nativeElement, 'color', null)
+    }
 }
