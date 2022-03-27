17afdc90f8bf9df5419fdcfc2dbeab50473d4b7a
Murat <mrdudov@gmail.com>
2022-03-27 16:51:39 +0300

    directives create custom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StyleDirective } from './directives/style.directive';
+import { IfnotDirective } from './directives/ifnot.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     StyleDirective,
+    IfnotDirective,
   ],
   imports: [
     BrowserModule,
