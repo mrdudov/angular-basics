33a611d467db952ac87dad81211064a6d5e7fd54
Murat <mrdudov@gmail.com>
2022-03-27 14:01:58 +0300

    directives create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
-export interface Post {
-  title: string
-  text: string
-  id?: number
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  posts:(Post | undefined)[] = [
-    {title: 'Title 1', text: 'text 1', id:1},
-    {title: 'Title 2', text: 'text 2', id:2}
-  ]
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log('Timeout')
-      this.posts[0]! = {
-        title: 'Changed!',
-        text: 'Changed text',
-        id: 4
-      }
-    }, 5000)
-  }
-
-  updatePosts(post?: Post) {
-    this.posts.unshift(post)
-  }
-
-  removePost(id: number) {
-    this.posts = this.posts.filter(p => p?.id !== id)
-  }
-
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
+import { StyleDirective } from './directives/style.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent,
+    StyleDirective,
   ],
   imports: [
     BrowserModule,
