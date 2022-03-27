b939efbab2998c69df180edbaf1a860ee340a702
Murat <mrdudov@gmail.com>
2022-03-27 20:28:30 +0300

    pipes currency, percent, json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,17 @@ export class AppComponent {
 
   date: Date = new Date()
 
+  float = 0.42
+
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  }
+
 }
