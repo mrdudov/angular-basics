84c6059d7ffbe09a9c10517362919b437b3422ab
Murat <mrdudov@gmail.com>
2022-03-29 19:32:14 +0300

    HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,15 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { SwitchComponent } from './switch/switch.component'
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SwitchComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
   ],
   providers: [
 
