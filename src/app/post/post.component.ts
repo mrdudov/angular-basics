199439a223813c0e41e70b54164fef12004dc224
Murat <mrdudov@gmail.com>
2022-03-30 23:18:13 +0300

    routing handling params

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,31 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Post, PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
+  post: Post | undefined
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private postsService: PostsService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log(params)
+      this.post = this.postsService.getById(parseInt(params['id']))
+    })  
+  }
+
+  loadPost() {
+    this.router.navigate(['/posts', 44])
+  }
 
 }
