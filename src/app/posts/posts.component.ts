2a43c0d75c91163c5ea046dd733f54c3e96d3ba8
Murat <mrdudov@gmail.com>
2022-03-31 00:34:23 +0300

    router queryParams fragment

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import { ActivatedRoute, Params, Router } from '@angular/router'
 import {PostsService} from '../posts.service'
 
 @Component({
@@ -6,6 +7,33 @@ import {PostsService} from '../posts.service'
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit{
+
+  showIds = false
+
+  constructor(
+    public postsService: PostsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.showIds = params['showIds']
+    })
+
+    this.route.fragment.subscribe(fragment => {
+      console.log('fragment', fragment)
+    })
+  }
+
+  showIdsProgram() {
+    this.router.navigate(['/posts'], {
+      queryParams: {
+        showIds: true
+      },
+      fragment: 'program-fragment'
+    })
+  }
+
 }
