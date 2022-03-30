4cea051733a3b14d2ce089dae7fc4be280518d0b
Murat <mrdudov@gmail.com>
2022-03-30 20:15:42 +0300

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,67 +1,11 @@
-import { Component, OnInit } from '@angular/core'
-import { delay } from 'rxjs';
-import { Todo, TodosService } from './todos.service';
-
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  todos: Todo[] = []
-
-  loading = false
-
-  todoTitle = ''
-
-  error = ''
-
-  constructor(private todosService: TodosService) {}
-
-  ngOnInit(): void {
-    this.fetchTodos()
-  }
-
-  addTodo() {
-    if(!this.todoTitle.trim()){
-      return
-    }
-
-    this.todosService.addTodo({
-      title: this.todoTitle,
-      complited: false
-    }).subscribe(todo => {
-      this.todos.push(todo)
-      this.todoTitle = ''
-    })
-
-  }
-
-  fetchTodos() {
-    this.loading = true
-    this.todosService.fetchTodo()
-      .subscribe(todos => {
-        this.todos = todos!
-        this.loading = false
-      }, error => {
-        this.error = error.message
-      })
-  }
-
-  removeTodo(id: number | undefined) {
-    this.todosService.removeTodo(id)
-      .subscribe(() => {
-        this.todos = this.todos.filter(t => t.id !== id)
-      })
-  }
-
-  compliteTodo(id: number | undefined) {
-    this.todosService.compliteTodo(id).subscribe(todo => {
-      console.log(todo)
-      this.todos.find(t => t.id === todo.id)!.complited = true
-    })
-  }
-
+export class AppComponent {
+  constructor() {}
 }
+
