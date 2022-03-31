a35bdd620aafb0fba409e679cac04f2b0e81eb92
Murat <mrdudov@gmail.com>
2022-03-31 21:04:57 +0300

    routing page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AboutExtraComponent } from "./about-extra/about-extra.component";
 import { AboutComponent } from "./about/about.component";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 import { HomeComponent } from "./home/home.component";
 import { PostComponent } from "./post/post.component";
 import { PostsComponent } from "./posts/posts.component";
@@ -12,7 +13,9 @@ const routes: Routes = [
         {path: 'extra', component: AboutExtraComponent}
     ]},
     {path: 'posts', component: PostsComponent},
-    {path: 'posts/:id', component: PostComponent}
+    {path: 'posts/:id', component: PostComponent},
+    {path: 'error', component: ErrorPageComponent},
+    {path: '**', redirectTo: '/error'}
 ]
 
 @NgModule({
