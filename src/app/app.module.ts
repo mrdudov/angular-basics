1aa64b82037f283389a11d114a2d5d16014da334
Murat <mrdudov@gmail.com>
2022-04-01 10:58:15 +0300

    dynamic components part 1 prepare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
