808ac8d94e0c2bbaa8553302fd0fedb2e6c2071d
Murat <mrdudov@gmail.com>
2022-04-01 22:26:01 +0300

    Unit tests test component class

---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,21 @@
+import { CounterComponent } from "./counter.component"
+
+describe('CounterComponent', () => {
+
+    let component: CounterComponent
+
+    beforeEach(() => {
+        component = new CounterComponent()
+    })
+    
+    it('should increment counter by 1', () => {
+        component.increment()
+        expect(component.counter).toBe(1)
+    })
+
+    it('should decriment counter by 1', () => {
+        component.decrement()
+        expect(component.counter).toBe(-1)
+    })
+    
+})
