5ef2630b75fde561f9b9521c2e03bda603b73ae5
Murat <mrdudov@gmail.com>
2022-04-01 11:40:25 +0300

    dynamic components part 2. ComponentFactoryResolver deprecatedgit add .!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core'
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
+import { ModalComponent } from './modal/modal.component'
+import { RefDirective } from './ref.directive'
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,22 @@ import {Component} from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
-  modal = false
 
-  constructor() {}
+  @ViewChild(RefDirective) refDir: RefDirective | undefined
+  
+  constructor(
+    private resolver: ComponentFactoryResolver
+  ) {}
+
+  showModal() {
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
+    this.refDir!.containerRef.clear()
+    const component = this.refDir!.containerRef.createComponent(modalFactory)
+    component.instance.title = 'Dinamic title'
+    component.instance.close.subscribe(() => {
+      this.refDir!.containerRef.clear()
+    })
+  }
+
 }
 
