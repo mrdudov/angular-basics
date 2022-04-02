17aa9e631664d7b1b96ba991962c2785aff84b5e
Murat <mrdudov@gmail.com>
2022-04-02 18:43:11 +0300

    Unit tests test delete post

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -42,4 +42,18 @@ describe('PostsComponent', () => {
     expect(component.message).toBe(error)
   })
 
+  it('should remove post if user confirms', () => {
+    const spy = spyOn(service, 'remove').and.returnValue(EMPTY)
+    spyOn(window, 'confirm').and.returnValue(true)
+    component.delete(10)
+    expect(spy).toHaveBeenCalledWith(10)
+  })
+
+  it('should NOT remove post if user doesnt confirm', () => {
+    const spy = spyOn(service, 'remove').and.returnValue(EMPTY)
+    spyOn(window, 'confirm').and.returnValue(false)
+    component.delete(10)
+    expect(spy).not.toHaveBeenCalled()
+  })
+
 })
