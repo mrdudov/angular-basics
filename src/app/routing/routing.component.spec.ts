94a858eb5d160bdfa50362d5cd14fc71c27578d2
Murat <mrdudov@gmail.com>
2022-04-03 12:31:42 +0300

    Integration tests. test router

--- src/app/routing/routing.component.spec.ts
+++ src/app/routing/routing.component.spec.ts
@@ -1,7 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
 
 import { RoutingComponent } from './routing.component';
+
+class RouterStub {
+  navigate(path: string[]) {}
+}
+
+class ActivatedRouteStab {
+  params: Observable<Params> = of({someParam: 'value'})
+}
 
 describe('RoutingComponent', () => {
   let component: RoutingComponent;
@@ -10,7 +20,11 @@ describe('RoutingComponent', () => {
   beforeEach(() => {
      TestBed.configureTestingModule({
       declarations: [ RoutingComponent ],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule],
+      providers: [
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouteStab},
+      ]
     })
 
     fixture = TestBed.createComponent(RoutingComponent);
@@ -21,5 +35,12 @@ describe('RoutingComponent', () => {
   it('should be defined', () => {
     expect(component).toBeDefined();
   });
+
+  it('shold navigate to posts if go back', () => {
+    let router= TestBed.get(Router)
+    let spy = spyOn(router, 'navigate')
+    component.goBack()
+    expect(spy).toHaveBeenCalledWith(['/posts'])
+  })
 
 });
