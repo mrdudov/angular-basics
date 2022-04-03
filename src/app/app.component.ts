7a5a8d5580d602ccd0d1e454446709c3187c960d
Murat <mrdudov@gmail.com>
2022-04-03 19:57:50 +0300

    Animations. state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('box', [
+      state('start', style({background: 'blue'})),
+      state('end', style({
+        background: 'red',
+        transform: 'scale(1.2)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  boxState = 'start'
+  
+  runAnimation() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+  }
+
 }
