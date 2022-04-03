bea8b87963279020d64ba18218b2f5295785e092
Murat <mrdudov@gmail.com>
2022-04-03 20:19:47 +0300

    Animations. nested animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,20 @@ import { Component } from '@angular/core';
         background: 'red',
         transform: 'scale(1.2)'
       })),
+      state('special', style({
+        background: 'orange',
+        transform: 'scale(0.5)',
+        borderRadius: '50%'
+      })),
       transition('start => end', animate(450)),
-      transition('end => start', animate('800ms ease-in-out'))
+      transition('end => start', animate('800ms ease-in-out')),
+      transition('special <=> *', [
+        style({background:'green'}),
+        animate(1000, style({
+          background: 'pink'
+        })),
+        animate(750)
+      ]),
     ])
   ]
 })
