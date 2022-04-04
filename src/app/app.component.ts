3d1e3be9cc53211fa50f2cf346b487db8015ff96
Murat <mrdudov@gmail.com>
2022-04-04 11:52:12 +0300

    RESET PROJECT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,10 @@
 import { Component } from '@angular/core';
-import { boxAnimation } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [ boxAnimation ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  boxState = 'start'
-  visible = true
-
-  runAnimation() {
-    this.boxState = this.boxState === 'end' ? 'start' : 'end'
-  }
-
-  animationStart(event: any) {
-    console.log('animationStart', event)
-  }
-
+  title = 'angular-basics';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AnimateComponent } from './animate/animate.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AnimateComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
