c529e2a43279a63c101257670ff43d62b91bd363
Murat <mrdudov@gmail.com>
2022-04-06 20:55:46 +0300

    blog. create post

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Post } from 'src/app/shared/interfaces';
+import { PostsService } from 'src/app/shared/posts.service';
 
 @Component({
   selector: 'app-create-page',
@@ -11,7 +12,9 @@ export class CreatePageComponent implements OnInit {
 
   form: FormGroup
 
-  constructor() {
+  constructor(
+    private postsService: PostsService
+  ) {
     this.form = new FormGroup({
       title: new FormControl(null, [Validators.required]),
       author: new FormControl(null, [Validators.required]),
@@ -33,7 +36,10 @@ export class CreatePageComponent implements OnInit {
       author: this.form.value.author,
       date: new Date()
     }
-    console.log(post)
+    this.postsService.create(post).subscribe(() => {
+      this.form.reset()
+    })
+
   }
 
 }
