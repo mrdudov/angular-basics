0ca569e7d3b0a4e7c7edc793f333530da2f76204
Murat <mrdudov@gmail.com>
2022-04-07 18:10:42 +0300

    blog. post edit service

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { Post } from 'src/app/shared/interfaces';
 import { PostsService } from 'src/app/shared/posts.service';
 
@@ -10,9 +10,12 @@ import { PostsService } from 'src/app/shared/posts.service';
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup | undefined
+  post: Post | undefined
+  submitted: boolean = false 
+  uSub: Subscription | undefined
 
   constructor(
     private route: ActivatedRoute,
@@ -24,6 +27,7 @@ export class EditPageComponent implements OnInit {
       .pipe(switchMap((params: Params) => {
         return this.postsService.getById(params['id'])
       })).subscribe((post: Post) => {
+        this.post = post
         this.form = new FormGroup({
           title: new FormControl(post.title, Validators.required),
           text: new FormControl(post.text, Validators.required)
@@ -31,8 +35,29 @@ export class EditPageComponent implements OnInit {
       })
   }
 
-  submit() {
+  ngOnDestroy(): void {
+    if(this.uSub) {
+      this.uSub.unsubscribe() 
+    }
+  }
 
+  submit() {
+    if (this.form?.invalid) {
+      return
+    }
+
+    this.submitted = true
+
+    this.uSub =  this.postsService.update({
+      id: this.post!.id,
+      text: this.form?.value.text,
+      title: this.form?.value.title,
+      author: this.post!.author,
+      date: this.post!.date
+    }).subscribe(() => {
+      this.submitted = false
+
+    })
   }
 
 }
